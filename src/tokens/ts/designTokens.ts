44abed7065535c374728ddfb137d400d8ea19946
Deniss Gavrilovs <denisgavriloff@outlook.com>
2023-04-05 08:35:09 +0300

    runs 'figma-sync' script for the first time

---
+++ src/tokens/ts/designTokens.ts
@@ -0,0 +1,9 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 05 Apr 2023 05:32:49 GMT
+ */
+
+export const colorWhite = "#fff";
+export const colorBlack = "#000";
+export const colorPrimary = "#f0f";
+export const tokenSetOrder0 = "global";
