af09f30d771ee4e6e81ed9dcc44caa05fd20582c
Deniss Gavrilovs <denisgavriloff@outlook.com>
2023-04-12 12:36:35 +0300

    returns Vite favicon icon and adds global font family

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/vendor/reset.css';
 import './tokens/css/design-tokens.css';
+import './main.css';
 import App from './app/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
